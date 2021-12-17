b8449956c6a694479ebfc945cd589b5d777fc299
Ethan Johnson <ethan.t.johnson1417@gmail.com>
2021-12-17 11:51:50 -0700

    Tables complete

--- allspice/dbSetup.sql
+++ allspice/dbSetup.sql
@@ -6,3 +6,45 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+CREATE TABLE recipes(
+  id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+  create_time DATETIME COMMENT 'Create Time',
+  update_time DATETIME COMMENT 'Update Time',
+  title TEXT NOT NULL,
+  subtitle TEXT NOT NULL,
+  category TEXT NOT NULL,
+  creatorId VARCHAR(255) NOT NULL,
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) DEFAULT CHARSET UTF8 COMMENT '';
+CREATE TABLE ingredients(
+  create_time DATETIME COMMENT 'Create Time',
+  update_time DATETIME COMMENT 'Update Time',
+  inName TEXT NOT NULL,
+  quantity TEXT NOT NULL,
+  recipeId INT NOT NULL,
+  FOREIGN KEY (recipeId) REFERENCES recipes(id) ON DELETE CASCADE
+) DEFAULT CHARSET UTF8 COMMENT '';
+CREATE TABLE steps(
+  create_time DATETIME COMMENT 'Create Time',
+  update_time DATETIME COMMENT 'Update Time',
+  stepOrder INT NOT NULL,
+  bodyText TEXT NOT NULL,
+  recipeId INT NOT NULL,
+  FOREIGN KEY (recipeId) REFERENCES recipes(id) ON DELETE CASCADE
+) DEFAULT CHARSET UTF8 COMMENT '';
+CREATE TABLE favorites(
+  create_time DATETIME COMMENT 'Create Time',
+  update_time DATETIME COMMENT 'Update Time',
+  accountId INT NOT NULL,
+  recipeId INT NOT NULL,
+  FOREIGN KEY (recipeId) REFERENCES recipes(id) ON DELETE CASCADE,
+  FOREIGN KEY (recipeId) REFERENCES recipes(id) ON DELETE CASCADE
+) DEFAULT CHARSET UTF8 COMMENT '';
+CREATE TABLE tries(
+  create_time DATETIME COMMENT 'Create Time',
+  update_time DATETIME COMMENT 'Update Time',
+  accountId INT NOT NULL,
+  recipeId INT NOT NULL,
+  FOREIGN KEY (recipeId) REFERENCES recipes(id) ON DELETE CASCADE,
+  FOREIGN KEY (recipeId) REFERENCES recipes(id) ON DELETE CASCADE
+) DEFAULT CHARSET UTF8 COMMENT '';
